33f35f419ee7fe6ed55986e00106d0b3ad68add8
Igor Kroshkin <igor.krosh@gmail.com>
2021-02-25 19:16:13 +0300

    FIX: Исправлена ошибка с выгрузкой только jpg изображений

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ let path = {
         pug: `${sourceFolder}/*.pug`,
         css: [`${sourceFolder}/scss/style.scss`, `${sourceFolder}/scss/bundle.scss`],
         js: [`${sourceFolder}/js/index.js`],
-        img: `${sourceFolder}/images/**/*.{jpg, png, svg, gif, ico, webp}`,
+        img: `${sourceFolder}/images/**/*.+(png|jpg|gif|ico|svg|webp)`,
         fonts: `${sourceFolder}/fonts/*.ttf`
     },
     watch: {
@@ -24,7 +24,7 @@ let path = {
         pug: `${sourceFolder}/**/*.pug`,
         css: `${sourceFolder}/scss/**/*.scss`,
         js: `${sourceFolder}/js/**/*.js`,
-        img: `${sourceFolder}/images/**/*.{jpg, png, svg, gif, ico, webp}`,
+        img: `${sourceFolder}/images/**/*.+(png|jpg|gif|ico|svg|webp)`,
     },
     clean: `./${projectFolder}/`
 }
