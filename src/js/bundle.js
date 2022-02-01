6fa4949dadfdb0cd06d59fb154193083f42e0732
Igor Kroshkin <igor.krosh@gmail.com>
2022-02-01 12:47:07 +0300

    Добавил доп. пакеты в сборку. owl.carousel - слайдер simple-lightbox - Лайтбокс для фото jquery-validation - проверка форм jquery-mask-plugin - маска для input

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,1 +1,14 @@
-global.$ = require('jquery');
+global.$ = global.jQuery = require('jquery');
+
+//require('owl.carousel');
+
+/*
+global.SimpleLightbox = require('simple-lightbox');
+global.SimpleLightbox.registerAsJqueryPlugin($);
+*/
+
+/*
+require('jquery-validation');
+require('jquery-validation/dist/localization/messages_ru');
+require('jquery-mask-plugin');
+*/
